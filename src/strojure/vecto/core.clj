5c8416dbc6e502a88190d1839ff614868bf82d61
Sergey Trofimov <sergey@trofimov.name>
2022-09-02 10:36:04 +0200

    Implement `remove-at` function

--- src/strojure/vecto/core.clj
+++ src/strojure/vecto/core.clj
@@ -1,5 +1,5 @@
 (ns strojure.vecto.core
-  (:import (clojure.lang ITransientCollection Indexed)))
+  (:import (clojure.lang ITransientCollection Indexed PersistentVector)))
 
 (set! *warn-on-reflection* true)
 (set! *unchecked-math* :warn-on-boxed)
@@ -8,17 +8,34 @@
 
 (defn insert
   "Inserts `obj` in the collection at index `idx` returning persistent vector."
-  {:arglists '([coll at obj])}
+  {:arglists '([coll idx obj])}
   [^Indexed coll, ^long idx, obj]
   (let [len (.count coll)]
     (if (= idx len)
       (conj coll obj)
       (let [xi (.nth coll (unchecked-int idx))]
-        (loop [k 0, ^ITransientCollection v! (transient [])]
+        (loop [i 0, ^ITransientCollection v! (transient [])]
           (cond
-            (= k len) (let [m (meta coll)]
+            (= i len) (let [m (meta coll)]
                         (cond-> (.persistent v!) m (with-meta m)))
-            (= k idx) (recur (unchecked-inc k) (-> v! (.conj obj) (.conj xi)))
-            :else,,,, (recur (unchecked-inc k) (-> v! (.conj (.nth coll (unchecked-int k)))))))))))
+            (= i idx) (recur (unchecked-inc i) (-> v! (.conj obj) (.conj xi)))
+            :else,,,, (recur (unchecked-inc i) (-> v! (.conj (.nth coll (unchecked-int i)))))))))))
+
+;;,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
+
+(defn remove-at
+  "Removes element from the the collection at index `idx` returning persistent
+  vector."
+  {:arglists '([coll idx])}
+  [^PersistentVector v, ^long idx]
+  (let [last-i (unchecked-dec (.count v))]
+    (if (= idx last-i)
+      (.pop v)
+      (loop [i idx, v! (.pop (.asTransient v))]
+        (if (= i last-i)
+          (let [m (.meta v)]
+            (cond-> (.persistent v!) m (with-meta m)))
+          (let [k (unchecked-inc i)]
+            (recur k (.assocN v! (unchecked-int i) (.nth v (unchecked-int k))))))))))
 
 ;;,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
--- test/strojure/vecto/core_test.clj
+++ test/strojure/vecto/core_test.clj
@@ -26,12 +26,37 @@
       (thrown? IndexOutOfBoundsException (vec/insert [1 2 3 4 5] 6 x))))
 
   (testing "Preserve metadata on insert."
-    (test/are [form] (::meta (meta form))
-      (vec/insert ^::meta [1 2 3 4 5] 2 x)
-      (vec/insert ^::meta [1 2 3 4 5] 2 x)
-      (vec/insert ^::meta [1 2 3 4 5] 0 x)
-      (vec/insert ^::meta [1 2 3 4 5] 5 x)
-      (vec/insert ^::meta [] 0 x)))
+    (test/are [form] form
+      (= {::meta true} (meta (vec/insert ^::meta [1 2 3 4 5] 2 x)))
+      (= {::meta true} (meta (vec/insert ^::meta [1 2 3 4 5] 0 x)))
+      (= {::meta true} (meta (vec/insert ^::meta [1 2 3 4 5] 5 x)))
+      (= {::meta true} (meta (vec/insert ^::meta [] 0 x)))))
+
+  )
+
+;;,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
+
+(deftest remove-test
+
+  (testing "Removing element at index."
+    (test/are [form] form
+      (vector?,,,, (vec/remove-at [1 2 3 4 5] 0))
+      (= [2 3 4 5] (vec/remove-at [1 2 3 4 5] 0))
+      (= [1 2 3 4] (vec/remove-at [1 2 3 4 5] 4))
+      (= [1 2 4 5] (vec/remove-at [1 2 3 4 5] 2))
+      (= [],,,,,,, (vec/remove-at [1] 0))))
+
+  (testing "Removing element out of bounds."
+    (test/are [form] form
+      (thrown? IndexOutOfBoundsException (vec/remove-at [1 2 3 4 5] -1))
+      (thrown? IndexOutOfBoundsException (vec/remove-at [1 2 3 4 5] 5))))
+
+  (testing "Preserve metadata on remove."
+    (test/are [form] form
+      (= {::meta true} (meta (vec/remove-at ^::meta [1 2 3 4 5] 0)))
+      (= {::meta true} (meta (vec/remove-at ^::meta [1 2 3 4 5] 4)))
+      (= {::meta true} (meta (vec/remove-at ^::meta [1 2 3 4 5] 2)))
+      (= {::meta true} (meta (vec/remove-at ^::meta [1] 0)))))
 
   )
 
