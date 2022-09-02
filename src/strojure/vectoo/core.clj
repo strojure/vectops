738cea093beefcc4b7bb2f9ba60e91f3160f2d1f
Sergey Trofimov <sergey@trofimov.name>
2022-09-02 16:08:14 +0200

    Vary implementations using protocols

--- src/strojure/vectoo/core.clj
+++ src/strojure/vectoo/core.clj
@@ -1,41 +1,68 @@
 (ns strojure.vectoo.core
-  (:import (clojure.lang ITransientCollection Indexed PersistentVector)))
+  (:import (clojure.lang ITransientVector PersistentVector)))
 
 (set! *warn-on-reflection* true)
 (set! *unchecked-math* :warn-on-boxed)
 
 ;;,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
 
-(defn insert-at
-  "Inserts `obj` in the collection at index `idx` returning persistent vector."
-  {:arglists '([coll idx obj])}
-  [^Indexed coll, ^long idx, obj]
-  (let [len (.count coll)]
-    (if (= idx len)
-      (conj coll obj)
-      (let [xi (.nth coll (unchecked-int idx))]
-        (loop [i 0, ^ITransientCollection v! (transient [])]
-          (cond
-            (= i len) (let [m (meta coll)]
-                        (cond-> (.persistent v!) m (with-meta m)))
-            (= i idx) (recur (unchecked-inc i) (-> v! (.conj obj) (.conj xi)))
-            :else,,,, (recur (unchecked-inc i) (-> v! (.conj (.nth coll (unchecked-int i)))))))))))
+(defprotocol VectorOps
+  (insert-at [coll, idx, obj]
+    "Inserts `obj` in the collection at index `idx`.")
+  (remove-at [coll, idx]
+    "Removes element from the the collection at index `idx`."))
 
 ;;,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
 
-(defn remove-at
-  "Removes element from the the collection at index `idx` returning persistent
-  vector."
-  {:arglists '([coll idx])}
-  [^PersistentVector v, ^long idx]
-  (let [last-i (unchecked-dec (.count v))]
+(defn- transient-insert-at
+  [^ITransientVector v!, ^long idx, obj]
+  (let [len (.count v!)]
+    (if (= idx len)
+      (.conj v! obj)
+      (loop [i idx, obj obj, v! v!]
+        (if (= i len)
+          (.conj v! obj)
+          (let [next-obj (.nth v! (unchecked-int i))]
+            (recur (unchecked-inc i) next-obj
+                   (.assocN v! (unchecked-int i) obj))))))))
+
+(defn- transient-remove-at
+  [^ITransientVector v!, ^long idx]
+  (let [last-i (unchecked-dec (.count v!))]
     (if (= idx last-i)
-      (.pop v)
-      (loop [i idx, v! (.pop (.asTransient v))]
+      (.pop v!)
+      (loop [i idx, v! v!]
         (if (= i last-i)
-          (let [m (.meta v)]
-            (cond-> (.persistent v!) m (with-meta m)))
+          (.pop v!)
           (let [k (unchecked-inc i)]
-            (recur k (.assocN v! (unchecked-int i) (.nth v (unchecked-int k))))))))))
+            (recur k (.assocN v! (unchecked-int i) (.nth v! (unchecked-int k))))))))))
+
+(extend-type PersistentVector VectorOps
+
+  (insert-at [v, ^long idx, obj]
+    (if (= idx (.count v))
+      (.cons v obj)
+      (let [m (.meta v)]
+        (-> (.asTransient v)
+            (transient-insert-at idx obj)
+            (persistent!)
+            (cond-> m (with-meta m))))))
+
+  (remove-at [v, ^long idx]
+    (if (= idx (unchecked-dec (.count v)))
+      (.pop v)
+      (let [m (.meta v)]
+        (-> (.asTransient v)
+            (transient-remove-at idx)
+            (persistent!)
+            (cond-> m (with-meta m)))))))
+
+(extend-type ITransientVector VectorOps
+  (insert-at [this, ^long idx, obj] (transient-insert-at this idx obj))
+  (remove-at [this, idx] (transient-remove-at this idx)))
+
+(extend-type nil VectorOps
+  (insert-at [_, idx, obj] (insert-at [] idx obj))
+  (remove-at [_, idx] (remove-at [] idx)))
 
 ;;,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
